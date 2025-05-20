f62ceb2a95c6d4ecddf62fbc9c60e0ef6348440a
JBernal <j.bernal@upm.es>
2025-05-20 08:48:54 +0200

    refactored logger

--- src/main/java/es/upm/api/configurations/LoggingFilter.java
+++ src/main/java/es/upm/api/configurations/LoggingFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -15,7 +14,8 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Enumeration;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Component
@@ -27,44 +27,28 @@ public class LoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        log.debug("-------------------------------------------------------------------------------------------------");
-        log.debug("Request: {} {}", request.getMethod(), request.getRequestURI());
-        log.debug("Headers:");
-        Collections.list(request.getHeaderNames()).forEach(headerName -> {
-            String headerValue = request.getHeader(headerName);
-            log.debug("  {}: {}", headerName, headerValue);
-        });
-        log.debug("Parameters:");
-        Enumeration<String> parameterNames = request.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            String paramValue = request.getParameter(paramName);
-            log.debug("  {}: {}", paramName, paramValue);
-        }
-
+        log.debug(">>>>> {} {}", request.getMethod(), request.getRequestURI());
+        Map<String, String> headerMap = Collections.list(request.getHeaderNames()).stream()
+                .collect(Collectors.toMap(name -> name, request::getHeader));
+        log.debug("     Headers:{}", headerMap);
+        Map<String, String> parameterMap = Collections.list(request.getParameterNames()).stream()
+                .collect(Collectors.toMap(name -> name, request::getParameter));
+        log.debug("     Parameters:{}", parameterMap);
         ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
-
         try {
             filterChain.doFilter(wrappedRequest, wrappedResponse);
         } catch (Exception e) {
             log.error("Error during filter processing", e);
             throw e;
         }
-
         // Ahora que se ha procesado la request, se puede obtener el cuerpo
         byte[] requestArray = wrappedRequest.getContentAsByteArray();
-        if (requestArray.length > 0) {
-            String requestBody = new String(requestArray, wrappedRequest.getCharacterEncoding());
-            log.debug("Request body (JSON): {}", requestBody);
-        }
-
-        log.debug("-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -    -   -   -   -");
+        String requestBody = new String(requestArray, wrappedRequest.getCharacterEncoding());
+        log.debug("     Request body (JSON): {}", requestBody);
         byte[] responseArray = wrappedResponse.getContentAsByteArray();
-        if (responseArray.length > 0) {
-            String responseBody = new String(responseArray, response.getCharacterEncoding());
-            log.debug("Response body: {}", responseBody);
-        }
+        String responseBody = new String(responseArray, response.getCharacterEncoding());
+        log.debug("     Response body: {}", responseBody);
         wrappedResponse.copyBodyToResponse();
     }
 }
