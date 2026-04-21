ea19ef484b81462495d043c5bec3228249d216bc
JBernal <j.bernal@upm.es>
2026-04-21 19:43:02 +0200

    added consent IT. miw-upm/goa#145

--- src/test/java/es/upm/api/services/UserServiceIT.java
+++ src/test/java/es/upm/api/services/UserServiceIT.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class UserServiceTest {
+class UserServiceIT {
 
     @Autowired
     private UserService userService;
